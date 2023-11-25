8542a81d18b72aac6f415d0f5d66155fdb38cb6a
Shubham Shekhar Jha <jhashubhamshekhar@gmail.com>
2023-11-25 09:40:49 -0600

    fixed import error

--- diffmimic/mimic_envs/__init__.py
+++ diffmimic/mimic_envs/__init__.py
@@ -1,9 +1,9 @@
 from brax import envs
-from .humanoid_mimic import HumanoidMimic
-from .humanoid_mimic_train import HumanoidMimicTrain
+from .humanoid_mimic import Mimic
+from .humanoid_mimic_train import MimicTrain
 from . import pd_controller
 
 
 def register_mimic_env():
-    envs.register_environment('humanoid_mimic', HumanoidMimic)
-    envs.register_environment('humanoid_mimic_train', HumanoidMimicTrain)
+    envs.register_environment('mimic', Mimic)
+    envs.register_environment('mimic_train', MimicTrain)

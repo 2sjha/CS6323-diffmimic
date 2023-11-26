a85030e47623428ec6f6ec1c4a5b7cf6a36102c8
Shubham Shekhar Jha <jhashubhamshekhar@gmail.com>
2023-11-26 13:40:39 -0600

    merge visualize.py from main

--- visualize.py
+++ visualize.py
@@ -5,46 +5,12 @@ import streamlit.components.v1 as components
 import streamlit as streamlit
 from diffmimic.utils.io import deserialize_qp, serialize_qp
 from diffmimic.mimic_envs import setup_brax_envs
-from diffmimic.mimic_envs.humanoid_system_config import Humanoid_System_Config
 
 setup_brax_envs()
 
-<<<<<<< HEAD
-st.title('DiffMimic - Visualization')
-with st.expander("Readme"):
-    st.markdown(
-        'Input modes:'
-        '\n- File Path: input the local file path to the .npy trajectory file.'
-        '\n- Experiment Log: select trajectories by browsing experiment logs.'
-        '\n- Direct Upload: directly upload the .npy trajectory file.')
-
-
-def show_rollout_traj(rollout_traj, tag):
-    if len(rollout_traj.shape) == 3:
-        seed = st.slider(f'Random seed ({tag})', 0, rollout_traj.shape[1] - 1, 0)
-        rollout_traj = rollout_traj[:, seed]
-
-    rollout_qp = [deserialize_qp(rollout_traj[i]) for i in range(rollout_traj.shape[0])]
-    rollout_traj = serialize_qp(deserialize_qp(rollout_traj))
-
-    env = envs.get_environment(
-        env_name="mimic",
-        system_config=Humanoid_System_Config,
-        reference_traj=rollout_traj,
-        obs_type='timestamp',
-        cyc_len=None,
-        reward_scaling=1.,
-        rot_weight=1.,
-        vel_weight=0.,
-        ang_weight=0.
-    )
-    components.html(html.render(env.sys, rollout_qp), height=500)
-
-=======
 streamlit.title('CS6323 - Project')
 streamlit.subheader('Shubham Shekhar Jha (sxj220028)')
 streamlit.subheader('Vedant Sapra (vks220000)')
->>>>>>> main
 
 def main():
     uploaded_file = streamlit.file_uploader("Evaluated Model Trajectory")

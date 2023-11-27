ec1b12c235801ba889cbbc32327a324959845642
Shubham Shekhar Jha <jhashubhamshekhar@gmail.com>
2023-11-27 03:23:25 -0600

    minor changes

--- diffmimic/mimic_envs/__init__.py
+++ diffmimic/mimic_envs/__init__.py
@@ -5,5 +5,6 @@ from . import pd_controller
 
 
 def setup_brax_envs():
+    """Setup Brax Environments for Mimic Evaluation and Mimic Training"""
     envs.register_environment("mimic", Mimic)
     envs.register_environment("mimic_train", MimicTrain)
--- diffmimic/utils/io.py
+++ diffmimic/utils/io.py
@@ -7,13 +7,13 @@ def deserialize_qp(nparray) -> brax.QP:
     """
     Get QP from a trajectory numpy array
     """
-    num_bodies = nparray.shape[-1] // 13    # pos (,3) rot (,4) vel (,3) ang (,3)
+    num_bodies = nparray.shape[-1] // 13  # pos (,3) rot (,4) vel (,3) ang (,3)
     batch_dims = nparray.shape[:-1]
     slices = [num_bodies * x for x in [0, 3, 7, 10, 13]]
-    pos = jnp.reshape(nparray[..., slices[0]:slices[1]], batch_dims + (num_bodies, 3))
-    rot = jnp.reshape(nparray[..., slices[1]:slices[2]], batch_dims + (num_bodies, 4))
-    vel = jnp.reshape(nparray[..., slices[2]:slices[3]], batch_dims + (num_bodies, 3))
-    ang = jnp.reshape(nparray[..., slices[3]:slices[4]], batch_dims + (num_bodies, 3))
+    pos = jnp.reshape(nparray[..., slices[0] : slices[1]], batch_dims + (num_bodies, 3))
+    rot = jnp.reshape(nparray[..., slices[1] : slices[2]], batch_dims + (num_bodies, 4))
+    vel = jnp.reshape(nparray[..., slices[2] : slices[3]], batch_dims + (num_bodies, 3))
+    ang = jnp.reshape(nparray[..., slices[3] : slices[4]], batch_dims + (num_bodies, 3))
     return QP(pos=pos, rot=rot, vel=vel, ang=ang)
 
 
